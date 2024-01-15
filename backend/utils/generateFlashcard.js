239ba72e78ae4a378890a10b2212e8445b7bcd6d
shihabdider <shihabdider@gmail.com>
2024-01-15 14:31:48 -0500

    milestone: fully working mvp

--- backend/utils/generateFlashcard.js
+++ backend/utils/generateFlashcard.js
@@ -1,23 +1,11 @@
 const OpenAI = require('openai');
 
 const openai = new OpenAI();
+openai.api_key = process.env.OPENAI_API_KEY;
 
-test_text = `Jod Gumbaz is a complex of two tombs located in Bijapur, in the Indian state of Karnataka.[1] Built during the late Adil Shahi period and completed in 1687,[2] it contains the tombs of Khan Muhammad and his spiritual advisor Abdul Razzaq Qadiri. A third, smaller tomb is located towards the west of the complex.
+test_text = `But it’s good to begin thinking about   your motivation for starting a startup because it dictates the type of product you should launch.  The Power of Goals  To nail down what you want out of entrepreneurship, you need to decide on your goals. This is a process most people skip due to skepticism about the benefits of the process.  A study at Dominican University 3   revealed that the following 3  factors substantially increased someone’s chance of following  through on their goals:  1.   Written Goals   –   “ Those who wrote their goals accomplished significantly more than those who did not  write their goals.”  2.   Public Commitment   -   “…those who sent their  commitments to a friend accomplished significantly more than those who wrote action commitments or did  not write their goals.”  3.   Accountability   –   “…those who sent wee kly progress reports to their friend accomplished significantly more  than those who had unwritten goals…”  It may feel like you’re an exception; that you don’t need goals or accountability…but trust the science and give   it a shot. Spend 20 minutes making a list of the things you are hoping to accomplish by starting up. If you believe what was said above, it will make a big difference. Worst case, you waste 20 minutes of your time.  Remember that there is no single   best   path to success as a startup founder. Since you are deciding on a specific lifestyle and are making sacrifices to get there, it can look like almost  anything. Just be sure it’s what you want.  3   Summary of Recent Goals Research, by Gail Matthews, Ph.D., Dominican University`;
 
-Background
-The Jod Gumbaz dates back to the late Adil Shahi period. The tombs were erected for Khan Muhammad and his spiritual advisor Abdul Razzaq Qadiri.[3]
-
-Khan Muhammad "the traitor" was a military commander in service of the Adil Shahis, and had been bribed into switching allegiances in favour of the Mughals. This information was given to the Adil Shahi ruler by Afzal Khan, who was also in the field. Khan Muhammad was recalled to Bijapur, and was assassinated. Subsequently, the Mughal emperor Aurangzeb ordered that a mausoleum be built for him using the taxes paid by Bijapur to the Mughals for one year. Later, Khan Muhammad's son, entitled Khawas Khan, who was also executed for his allegiance to the Mughals, was also buried within the same tomb. The tomb of Abdul Razzaq Qadiri, who was Khan Muhammad's spiritual advisor was also built around the same time.[4]
-
-
-The Jod Gumbaz, described as "Tombs of Negro Nobles, Beejapoor", photographed by Henry Hinton, ca. 1855–1862
-During the British period, the tomb of Khan Muhammad was used as an office and dwelling for the executive engineer.[4][5] The compound was being used for the residence of the sessions judge of Bijapur up until 1918, when the Archaeological Survey of India cleared all encroachments.[6] Due to the reverence Muslims had for Abdul Razzaq, his tomb was not repurposed.[4] The complex is now protected as a monument of national importance.[7]
-
-
-The tomb of Khan Muhammad
-The tombs are located on a high elevation, because the vaults are located above ground level, which is usually not the case.[4] The tomb of Khan Muhammad is an octagonal building to the south of the complex.[4] A trefoil-patterned parapet rises above the hall, with finials at each corner.[8] It is surmounted by a dome, resting on a lotus-shaped base. Abdul Razzaq Qadiri's tomb is a square building to the north of the complex, and larger than the tomb of Khan Muhammad.[4] It is similar in design, with the trefoil patterned parapet, finials on the corners, and dome rising above a lotus-shaped base. This mausoleum is revered as a dargah.[1] A smaller mausoleum located towards the west of the Jod Gumbaz is the burial place of Sidi Rehan, an officer during the reign of Muhammad Adil Shah.[4]`;
-
-highlight = `During the British period, the tomb of Khan Muhammad was used as an office and dwelling for the executive engineer.`
+highlight = `A study at Dominican University3 revealed that the following 3factors substantially increased someone’s chance of followingthrough on their goals:1. Written Goals – “Those who wrote their goalsaccomplished significantly more than those who did notwrite their goals.”2. Public Commitment - “...those who sent theircommitments to a friend accomplished significantlymore than those who wrote action commitments or didnot write their goals.”3. Accountability – “...those who sent weekly progressreports to their friend accomplished significantly morethan those who had unwritten goals...”`
 
 async function generateFlashcards(highlight, page) {
   const completion = await openai.chat.completions.create({
@@ -32,7 +20,7 @@ async function generateFlashcards(highlight, page) {
                       { question: "Question 2", answer: "Answer 2" } 
                     ] 
                   }
-                  The flashcards should only be on the highlight. The page can be used for context`,
+                  The flashcards should only be on the highlight. The page can be used for context. Do not include commas in the answer.`,
       },
       {
         role: 'user',
@@ -43,7 +31,9 @@ async function generateFlashcards(highlight, page) {
     response_format: { type: 'json_object' },
   });
 
-  return completion.data.choices[0].message.content;
+  return completion.choices[0].message.content;
 }
+
+//generateFlashcards(highlight, test_text);
 
 module.exports.generateFlashcards = generateFlashcards;
